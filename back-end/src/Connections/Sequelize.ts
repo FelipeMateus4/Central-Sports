b90c0f27792a32ae374b7a0ad66d51d328f3747e
Felipe Mateus <ipemateusribeiro@gmail.com>
2024-07-30 08:23:05 -0300

    chore: test sequelize connection

--- back-end/src/Connections/Sequelize.ts
+++ back-end/src/Connections/Sequelize.ts
@@ -1,14 +1,19 @@
-import { Sequelize } from "sequelize";
-import { config } from "dotenv";
+import { Sequelize } from 'sequelize';
+import { config } from 'dotenv';
+import { log } from 'console';
 
 config();
 
 const sequelize = new Sequelize(
     process.env.DATABASE as string,
-    process.env.USERNAME as string,
+    process.env.USER as string,
     process.env.PASSWORD as string,
     {
-        host: "localhost",
-        dialect: "postgres",
+        host: 'localhost',
+        dialect: 'postgres',
     }
 );
+
+console.log(process.env.DATABASE);
+
+export default sequelize;
--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,6 +1,13 @@
 import { app } from "./app";
+import sequelize from "./Connections/Sequelize";
 
 const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(PORT, async () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
 });

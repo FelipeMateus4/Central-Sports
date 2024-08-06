5e0d8f96f483e27990d1c90c22a0fdc4a33bcaff
Felipe Mateus <ipemateusribeiro@gmail.com>
2024-08-06 18:05:54 -0300

    feat: create type user

---
+++ back-end/src/Types/UserType.ts
@@ -0,0 +1,13 @@
+import passport from 'passport';
+import { z } from 'zod';
+
+const user = z.object({
+    email: z.string(),
+    password: z.string(),
+    secret: z.string(),
+    tipo: z.string(),
+});
+
+type user = z.infer<typeof user>;
+
+export { user as userType };

a4f224022dd8a22b264c7678171cdf4191e6c491
Felipe Mateus <ipemateusribeiro@gmail.com>
2024-08-06 18:04:05 -0300

    feat: create type treinador

---
+++ back-end/src/Types/TreinadorTypes.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+const treinador = z.object({
+    id: z.number(),
+    nome: z.string(),
+    especialidade: z.string(),
+});
+
+type treinador = z.infer<typeof treinador>;
+
+export { treinador as treinadorType };

e2db327c2b163a8375294017414841ee9cc8d362
Felipe Mateus <ipemateusribeiro@gmail.com>
2024-08-22 03:28:06 -0300

    fix: bugs

--- Implementacao/front-end/src/Pages/Authenticate/Authenticate.js
+++ Implementacao/front-end/src/Pages/Authenticate/Authenticate.js
@@ -1,24 +1,76 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { AuthContext } from '../../Context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 import './Authenticate.css';
 
 const Authenticate = () => {
+    const [token, setToken] = useState('');
+    const [timer, setTimer] = useState(60);
+    const [error, setError] = useState(null);
+    const { loginAuth } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTimer((prevTimer) => (prevTimer > 0 ? prevTimer - 1 : 0));
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:5000/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+                body: JSON.stringify({ token: token }),
+            });
+
+            const data = await response.json();
+            console.log(data);
+
+            if (response.status === 200) {
+                loginAuth(data.user);
+                console.log('Logado com sucesso');
+                navigate('/authenticate');
+            } else if (response.status === 401) {
+                console.log('Não autorizado');
+                setError({ message: data.message });
+            }
+        } catch (error) {
+            setError({ message: error.message });
+            console.error('Erro ao tentar logar:', error);
+        }
+    };
+
     return (
-        <div className="page-section">
-            <section className="login-container">
-                <h1 className="text">Bem-vindo à Central Sports</h1>
-                <div className="line"></div>
-                <div className="forms">
-                    <form>
-                        <div className="campos">
-                            <input className="formss-input" type="text" placeholder="Email" />
-                            <input className="formss-input" type="password" placeholder="Senha" />
-                        </div>
-                        <button className="formss-button">Entrar</button>
-                    </form>
-                </div>
-            </section>
+        <div className="page-section-authenticate">
+            <div className="authenticate-container">
+                <h2 className="title">Seu Token</h2>
+                <p className="text-center timer-text">Seu código expira em: {timer} segundos</p>
+                {error && <div className="error">{error.message}</div>}
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="name"></label>
+                        <input
+                            placeholder="Token"
+                            type="text"
+                            id="name"
+                            value={token}
+                            onChange={(e) => setToken(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <button type="submit" className="btn-authenticate">
+                        Registrar
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };

d0f794fdc0cf8b928d8c80d3d8e89c3e312098a7
Felipe Mateus <ipemateusribeiro@gmail.com>
2024-08-22 03:35:41 -0300

    fix: protect routes

--- Implementacao/front-end/src/Routes/ProtectedRoute.js
+++ Implementacao/front-end/src/Routes/ProtectedRoute.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from '../Context/AuthContext';
 
-const ProtectedRoute = () => {
+const ProtectedRoute = ({ children }) => {
     const { isAuthenticated } = useContext(AuthContext);
+
     console.log(isAuthenticated);
+
     if (!isAuthenticated) {
         console.log(isAuthenticated);
         return <Navigate to="/" replace />;
     }
 
-    return <Outlet />;
+    return children;
 };
 
 export default ProtectedRoute;
